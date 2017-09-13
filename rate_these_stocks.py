d6de9951d3dda12762857a254838b2d354b91ce3
Rushi Gajaria <rushigajaria@gmail.com>
2017-09-12 21:59:08 -0400

    Update rate_these_stocks.py

--- rate_these_stocks.py
+++ rate_these_stocks.py
@@ -4,7 +4,7 @@ from yahoo_finance import Share
 print "Stock Symbols:",
 s = raw_input()
 companies = map(None, s.split())
-print(len(companies))
+# print(len(companies))
 
 stockprices = [[x for x in xrange(2)] for y in range(len(companies))]
 
@@ -12,7 +12,7 @@ i = 0
 for company in companies:
 	
 	information = Share(company)
-	print(company)
+# 	print(company)
 	stockprices[i][0] = company
 	volatility = float(information.get_year_high()) - float(information.get_year_low())
 	potential = float(information.get_EPS_estimate_next_year()) - float(information.get_earnings_share())
@@ -21,15 +21,3 @@ for company in companies:
 	i+=1
 	
 print sorted(stockprices,key=lambda x: (x[1]), reverse=True)
-	
-	
-	
-yahoo = Share('YHOO')
-print yahoo.get_open()
-'36.60'
-print yahoo.get_price()
-'36.84'
-print yahoo.get_trade_datetime()
-'2014-02-05 20:50:00 UTC+0000'
-
-print stockprices[0][1]
